aa1cff6632cee1a2ec243942a109938770c7b068
Boy Bung Bu <pham.an.20.09@gmail.com>
2017-09-08 08:22:33 +0700

    add function import questions from excel

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ require __DIR__ . '/app/routes.php';
 require 'object/Question.php';
 
 $app->get('/', function ($request, $response, $args) {
-    $response->write("Hello, ");
+    $this->view->render($response, '/home.twig');
     return $response;
-});
+})->setName('home');
 $app->get('/questions', function ($request, $response) {
 	$result =array();
 	try {			
@@ -64,6 +64,58 @@ $app->get('/questions/{id}', function ($request, $response) {
 		$result['status'] = "error";
 		return $response->withJson ( $result );
 	}
+});
+
+$app->post('/questions/import', function($request, $response,$arg) {   
+    $filepath =$_FILES["filepath"]["tmp_name"];   
+    $objPHPExcel = PHPExcel_IOFactory::load ( $filepath );  
+    $data = array();
+    $succ = 0;
+    $err = 0;   
+	$ActiveSheet = $objPHPExcel->getActiveSheet ();    
+	for($r = 3; $r <= $ActiveSheet->getHighestRow (); $r ++) {
+		try {           
+            if(( $ActiveSheet->getCell ( 'A' . $r ) == ''))
+            {
+                continue;
+            }            
+           
+			$mapping = array (	
+                'A' => 'content',			
+				'B' => 'answer1',
+				'C' => 'answer2',
+				'D' => 'answer3',				
+				'E' => 'correct_answer',					
+			);            
+           
+			for($col = 'A'; $col !== 'F'; $col ++) {               
+				$cell = $ActiveSheet->getCell ( $col . $r );
+				$text_value = trim ( $cell->getValue (), " " );               
+				switch ($col) {                    					
+					default :
+						$data [$mapping[$col]] = $text_value;
+                        break;
+				}
+            }  
+            //debug($data);      
+			$sth = $this->db->prepare ( "INSERT INTO questions(content, answer1, answer2, answer3, correct_answer) VALUES(:content, :answer1, :answer2, :answer3, :correct_answer)" );
+            $sth->bindParam ( 'content', $data['content'] );
+            $sth->bindParam ( 'answer1', $data['answer1'] );
+            $sth->bindParam ( 'answer2', $data['answer2'] );
+            $sth->bindParam ( 'answer3', $data['answer3'] );
+            $sth->bindParam ( 'correct_answer', $data['correct_answer'] );
+            $sth->execute ();	
+            $succ++;  
+            $this->logger->info("successfully imported");
+        } 
+        catch ( Exception $exc ) {
+            $this->logger->info($exc);
+            $err = 1;
+        }
+	}     
+	echo 'ok';
+    //$this->flash->addMessage('success', "Đã thêm thành công $succ câu hỏi.");
+   // return $response->withRedirect($this->router->pathFor('partner'), 200);
 });
 
 $app->run();
